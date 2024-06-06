ecfcf47df3423707f3b252b35b224821086e2959
carloschnouda <carlos.chnouda@gmail.com>
2024-06-06 14:12:00 +0300

    change video thumbnail poster color

--- resources/views/components/banner.blade.php
+++ resources/views/components/banner.blade.php
@@ -1,13 +1,13 @@
 <div class="video-container">
     <div class="h-full w-full">
         @if ($settings['banner_video'])
-            <video autoplay muted playsinline loop id="video-background" poster="{{asset('images/black-bg.webp')}}">
+            <video autoplay muted playsinline loop id="video-background" poster="{{ asset('images/poster.png') }}">
                 <source src="{{ Storage::url($settings['banner_video']) }}" type="video/mp4">
             </video>
-            <div class="mute-button absolute right-5 bottom-5 sm:right-10 sm:bottom-10 ">
-                <div class="mute-wrapper bg-[#A8D0CF] p-5 rounded-[50px]">
-                    <img class="mute w-5 h-5 " src="{{ asset('images/mute.svg') }}" alt="Mute Button">
-                    <img class="unmute h-5 w-5 hidden" src="{{ asset('images/unmute.svg') }}" alt="Unmute Button">
+            <div class="mute-button absolute bottom-5 right-5 sm:bottom-10 sm:right-10">
+                <div class="mute-wrapper rounded-[50px] bg-[#A8D0CF] p-5">
+                    <img class="mute h-5 w-5" src="{{ asset('images/mute.svg') }}" alt="Mute Button">
+                    <img class="unmute hidden h-5 w-5" src="{{ asset('images/unmute.svg') }}" alt="Unmute Button">
                 </div>
             </div>
         @else
